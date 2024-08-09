7a8b3f105882edeb896bf12dda30a94b5a9b4f93
Jonatan Männchen <jonatan@maennchen.ch>
2024-08-09 13:50:08 +0200

    Do not automatically start the resources (#38)

--- lib/senzing/application.ex
+++ lib/senzing/application.ex
@@ -3,52 +3,12 @@ defmodule Senzing.Application do
 
   use Application
 
-  alias Senzing.G2.ResourceInit
-
   @impl true
-  def start(_type, args) do
-    children =
-      case g2_start_opts(args) do
-        {:ok, options} -> [{ResourceInit, options}]
-        :error -> []
-      end
-
+  def start(_type, _args) do
     Supervisor.start_link(
-      [Senzing.Telemetry | children],
+      [Senzing.Telemetry],
       strategy: :one_for_one,
       name: Senzing.Supervisor
     )
-  end
-
-  @spec g2_start_opts(args :: Keyword.t()) :: {:ok, ResourceInit.options()} | :error
-  defp g2_start_opts(args) do
-    with {:ok, mod} <- Keyword.fetch(args, :mod) do
-      {:ok,
-       [mod: mod]
-       |> then(fn opts ->
-         case Keyword.fetch(args, :ini_params) do
-           :error -> opts
-           {:ok, params} -> Keyword.put(opts, :ini_params, params)
-         end
-       end)
-       |> then(fn opts ->
-         case Application.fetch_env(:senzing, :engine_name) do
-           :error -> opts
-           {:ok, name} -> Keyword.put(opts, :name, name)
-         end
-       end)
-       |> then(fn opts ->
-         case Application.fetch_env(:senzing, :verbose_logging?) do
-           :error -> opts
-           {:ok, verbose_logging?} -> Keyword.put(opts, :verbose_logging, verbose_logging?)
-         end
-       end)
-       |> then(fn opts ->
-         case Application.fetch_env(:senzing, :prime) do
-           :error -> opts
-           {:ok, prime?} -> Keyword.put(opts, :prime, prime?)
-         end
-       end)}
-    end
   end
 end
